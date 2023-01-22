c5472725cd3941d1b2149a01b57241371a81cf22
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-01-22 12:35:11 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./style.css"
+
+
+function reducer(state, action){
+
+}
 
 function App() {
+  const [{currentOperand, previosOperand, operation}, dispatch] = useReducer(reducer,{})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previos-operand">{previosOperand}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button className="">DEL</button>
+      <button className="">&divide;</button>
+      <button className="">1</button>
+      <button className="">2</button>
+      <button className="">3</button>
+      <button className="">*</button>
+      <button className="">4</button>
+      <button className="">5</button>
+      <button className="">6</button>
+      <button className="">+</button>
+      <button className="">7</button>
+      <button className="">8</button>
+      <button className="">9</button>
+      <button className="">-</button>
+      <button className="">.</button>
+      <button className="">0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
